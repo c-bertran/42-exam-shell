cecf281ed23bc12e39b86e90b105d0031aa679af
c-bertran <cbertran@student.42.fr>
2022-01-27 18:48:38 +0100

    Add iddqd

--- srcs/modules/commands/iddqd.js
+++ srcs/modules/commands/iddqd.js
@@ -1,8 +1,49 @@
 require('../fspatch');
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
 const http = require('http');
+
+const logo = (config) => `
+███████████████████    ██████████████   ██████████████   ██████    ██████████
+   █████████████████ █████████████████ █████████████████ ███████   ███████   
+   █████████████████ █████████████████ █████████████████ ███████  ████████   
+   █████████████████ █████████████████ █████████████████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████    ██████ ██████     ██████ ██████     ██████ █████████████████   
+   ███████   ███████ ███████    ██████ ██████    ███████ █████████████████   
+   █████████████████ ██████████ ██████ ██████ ██████████ ██████ ██ ███████   
+   █████████████████   ███████████████ ███████████████   ██████ ██ ███████   
+   ███████████████       █████████████ █████████████       ████    ███████   
+   █████████████            ████████     ████████            ██    ███████   
+   ██████████                  ███         ███                     ███████   
+   ███████                                                         ███████   
+   █████                                                             █████   
+   ██                                                                   ██
+                     is running on http://${config.host}:${config.port}
+`;
+
+const mimeType = {
+	'.ico': 'image/x-icon',
+	'.html': 'text/html',
+	'.js': 'text/javascript',
+	'.json': 'application/json',
+	'.css': 'text/css',
+	'.png': 'image/png',
+	'.jpg': 'image/jpeg',
+	'.wav': 'audio/wav',
+	'.mp3': 'audio/mpeg',
+	'.svg': 'image/svg+xml',
+	'.pdf': 'application/pdf',
+	'.doc': 'application/msword',
+	'.eot': 'application/vnd.ms-fontobject',
+	'.ttf': 'application/font-sfnt',
+	'.binary': 'binary/octet-stream',
+};
 
 class IDDQD
 {
@@ -10,30 +51,52 @@ class IDDQD
 	{
 		this.config = {
 			host: 'localhost',
-			port: '8080',
+			port: '1332',
 		};
+		this.servePath = path.join(__dirname, '..', '..', 'iddqd');
+		this.#initServer();
 	}
 
-	start()
+	#initServer()
 	{
 		this.instance = http.createServer((req, res) =>
 		{
-			fs.readFile(path.join(process.cwd(), 'srcs', 'IDDQD', 'index.html'), (err, data) =>
+			if (req.method === 'GET')
 			{
-				if (err)
+				const file = { URL: String, PATH: String, EXT: String };
+				file.URL = (req.url === '/') ? '/index.html' : req.url;
+				file.PATH = path.resolve(path.join(this.servePath, file.URL));
+				file.EXT = path.extname(file.PATH);
+
+				if (!Object.prototype.hasOwnProperty.call(mimeType, file.EXT))
+					file.EXT = '.binary';
+				fs.access(file.PATH, (err) =>
 				{
-					res.writeHead(404);
-					res.end(JSON.stringify(err));
-					return;
-				}
-				res.writeHead(200);
-				res.end(data);
-			});
+					if (err)
+					{
+						res.setHeader('Content-Type', 'text/html');
+						res.writeHead(404);
+						res.end('<h2>404</h2>');
+						return;
+					}
+					res.setHeader('Content-Type', mimeType[file.EXT]);
+					res.writeHead(200);
+					fs.createReadStream(file.PATH).pipe(res);
+				});
+			}
+			else
+			{
+				res.setHeader('Content-Type', 'text/html');
+				res.writeHead(404);
+				res.end('<h2>404</h2>');
+			}
 		});
-		this.instance.listen(this.config.port, this.config.host, () =>
-		{
-			console.log(`Server is running on http://${this.config.host}:${this.config.port}`);
-		});
+		this.instance.listen(this.config.port, this.config.host);
+	}
+
+	print()
+	{
+		console.log(logo(this.config));
 	}
 }
 
