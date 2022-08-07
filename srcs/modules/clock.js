f8af0ebdaba6a0c43637370c7847fbdb33e83e75
Clement bertrand <clement.bertrand22@gmail.com>
2022-08-08 00:02:52 +0200

    Lint index.js, correct bug inside grademe command

--- srcs/modules/clock.js
+++ srcs/modules/clock.js
@@ -45,7 +45,52 @@ function secondsToClock(secs)
 	return `${hours}:${minutes}:${seconds}`;
 }
 
+/**
+ * Transform string time to real time
+ * Accept days `d`, hours `h`, minutes `m`, seconds `s`
+ * Exemple: `3d 14h 41m 17s`
+ * @param {String} time String containing time information
+ * @returns {Object} Time parsed in array
+ */
+const convertTime = (time) =>
+{
+	const regex = /(?<days>\d*)d|(?<hours>\d*)h|(?<minutes>\d*)m|(?<seconds>\d*)s/gm;
+	const ret = {
+		days: Number(0),
+		hours: Number(0),
+		minutes: Number(0),
+		seconds: Number(0),
+		time: Number(0),
+	};
+
+	const match = Array.from(time.matchAll(regex), (m) => (
+		{
+			days: Number(m.groups.days),
+			hours: Number(m.groups.hours),
+			minutes: Number(m.groups.minutes),
+			seconds: Number(m.groups.seconds),
+		}
+	));
+	for (const obj of match)
+	{
+		if (!Number.isNaN(obj.days))
+			ret.days += obj.days;
+		if (!Number.isNaN(obj.hours))
+			ret.hours += obj.hours;
+		if (!Number.isNaN(obj.minutes))
+			ret.minutes += obj.minutes;
+		if (!Number.isNaN(obj.seconds))
+			ret.seconds += obj.seconds;
+	}
+	ret.time += ret.days * 86400;
+	ret.time += ret.hours * 3600;
+	ret.time += ret.minutes * 60;
+	ret.time += ret.seconds;
+	return ret;
+};
+
 module.exports = {
 	timer,
 	secondsToClock,
+	convertTime,
 };
