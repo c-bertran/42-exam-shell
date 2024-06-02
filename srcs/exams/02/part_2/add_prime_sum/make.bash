6b0b9221b13494e7623937c846ca55391406dff1
c-bertran <clement.bertrand22@gmail.com>
2024-06-02 23:59:38 +0200

    Patch issue #13

--- srcs/exams/02/part_2/add_prime_sum/make.bash
+++ srcs/exams/02/part_2/add_prime_sum/make.bash
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-clang -Wall -Werror -Wextra main.c -o addPrimeSub || exit 100
-./addPrimeSub > real
-./addPrimeSub one two >> real
-./addPrimeSub -10 >> real
-./addPrimeSub 1 >> real
-./addPrimeSub 24 >> real
-./addPrimeSub 78 >> real
-./addPrimeSub 404 >> real
+clang -Wall -Werror -Wextra main.c -o addPrimeSum || exit 100
+./addPrimeSum > real
+./addPrimeSum one two >> real
+./addPrimeSum -10 >> real
+./addPrimeSum 1 >> real
+./addPrimeSum 24 >> real
+./addPrimeSum 78 >> real
+./addPrimeSum 404 >> real
 
-clang -Wall -Werror -Wextra $1/add_prime_sub/add_prime_sub.c -o addPrimeSub2
-./addPrimeSub2 > fake
-./addPrimeSub2 one two >> fake
-./addPrimeSub2 -10 >> fake
-./addPrimeSub2 1 >> fake
-./addPrimeSub2 24 >> fake
-./addPrimeSub2 78 >> fake
-./addPrimeSub2 404 >> fake
+clang -Wall -Werror -Wextra $1/add_prime_sum/add_prime_sum.c -o addPrimeSum2
+./addPrimeSum2 > fake
+./addPrimeSum2 one two >> fake
+./addPrimeSum2 -10 >> fake
+./addPrimeSum2 1 >> fake
+./addPrimeSum2 24 >> fake
+./addPrimeSum2 78 >> fake
+./addPrimeSum2 404 >> fake
 
 diff -y --suppress-common-lines real fake > __diff
-bash leaks.bash addPrimeSub2 0 78 > /dev/null 2>&1
+bash leaks.bash addPrimeSum2 0 78 > /dev/null 2>&1
 
-rm -rf addPrimeSub addPrimeSub2 real fake
+rm -rf addPrimeSum addPrimeSum2 real fake
